2b606d28db0268a6a1382916df0c01e071203433
Josh Riley <joshjriley@gmail.com>
2019-09-18 13:17:05 -1000

    Adding conda environment file; update start_keck_viewers.bash; delete start_soundplay.bash

--- start_keck_viewers.bash
+++ start_keck_viewers.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
+
+# NOTE: The KRO environment is created with:
+#   conda env create -f environment.yml
+
 CMD="source activate KRO ; python get_vnc_sessions.py $@"
-xterm -title "Launch VNCs" -e "$CMD"
-# The KRO environment is created with:
-# conda create -n KRO python=3.6 yaml pyyaml paramiko astropy
+xterm -title "Launch Keck VNCs" -e "$CMD"
