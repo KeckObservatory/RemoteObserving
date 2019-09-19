83af821e0fe00671488357302aab2bc5e19f1943
Josh Riley <joshjriley@gmail.com>
2019-09-18 17:05:17 -1000

    Added cd to script path in bash script so it can be called from anywhere and/or added to path

--- start_keck_viewers.bash
+++ start_keck_viewers.bash
@@ -3,5 +3,7 @@
 # NOTE: The KRO environment is created with:
 #   conda env create -f environment.yml
 
-CMD="source activate KRO ; python keck_vnc_launcher.py $@"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+CMD="pwd; source activate KRO ; python keck_vnc_launcher.py $@"
 xterm -title "Launch Keck VNCs" -e "$CMD ; bash"
