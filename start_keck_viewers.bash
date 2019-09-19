0b6996143cb1ad8ee21ed68036d4426b41bec776
Josh Riley <joshjriley@gmail.com>
2019-09-18 14:06:02 -1000

    Fixed incorrect module call; Added bash command to end of xterm command so that it will keep the xterm open otherwise if the script errors and exits the xterm closes before they can see any error messages.

--- start_keck_viewers.bash
+++ start_keck_viewers.bash
@@ -3,5 +3,5 @@
 # NOTE: The KRO environment is created with:
 #   conda env create -f environment.yml
 
-CMD="source activate KRO ; python get_vnc_sessions.py $@"
-xterm -title "Launch Keck VNCs" -e "$CMD"
+CMD="source activate KRO ; python KeckVncLauncher.py $@"
+xterm -title "Launch Keck VNCs" -e "$CMD ; bash"
