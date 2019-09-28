d9699794fe9624fbfeac47d96d15923ecddac880
Josh Riley <joshjriley@gmail.com>
2019-09-27 14:10:21 -1000

    switch to conda activate instead of source activate.  Note that this took some special commands to get this to work without making the user configure conda correctly.

--- start_keck_viewers.bash
+++ start_keck_viewers.bash
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# NOTE: The KRO environment is created with:
-#   conda env create -f environment.yaml
+#activate conda environment
+# NOTE: The KRO environment is created with: conda env create -f environment.yaml
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate KRO
+#NOTE: old method using 'source' will work too but not preferred
+#source activeate KRO
 
-#change to script dir (in case this is called from elsewhere)
+#change to script dir (so we don't need full path to keck_vnc_launcher.py)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-#activate conda environment and run app with args
-source activate KRO
+#launch 
 python keck_vnc_launcher.py $@
 
 #NOTE: old method of launching in separate xterm; don't see advantage of doing it this way.
