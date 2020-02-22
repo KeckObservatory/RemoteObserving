237918823aebd4f3a7fe17870f1d0d460e0657e5
Josh Walawender <jmwalawender@gmail.com>
2020-02-21 16:10:21 -1000

    Handle ssh key more cleanly

--- test_basic_connectivity.py
+++ test_basic_connectivity.py
@@ -3,6 +3,7 @@ import sshtunnel
 import logging
 from getpass import getpass
 from keck_vnc_launcher import create_logger, KeckVncLauncher, create_parser
+import pytest
 
 
 # create kvl object
@@ -13,14 +14,24 @@ kvl.log_system_info()
 kvl.args = create_parser()
 kvl.get_config()
 kvl.check_config()
+if kvl.config.get('nosshkey', False) is True:
+    vnc_account = kvl.args.account
+    kvl.vnc_password = getpass(f"\nPassword for user {vnc_account}: ")
 
-servers_to_test = ['svncserver1', 'svncserver2', 'mosfire', 'hires', 'lris',
-                   'kcwi', 'nirc2', 'nires', 'nirspec']
+servers_and_results = [('svncserver1', 'kaalualu'),
+                       ('svncserver2', 'ohaiula'),
+                       ('mosfire', 'vm-mosfire'),
+                       ('hires', 'vm-hires'),
+                       ('lris', 'vm-lris'),
+                       ('kcwi', 'vm-kcwi'),
+                       ('nirc2', 'vm-nirc2'),
+                       ('nires', 'vm-nires'),
+                       ('nirspec', 'vm-nirspec')]
 
 def test_firewall_authentication():
     kvl.is_authenticated = False
     if kvl.do_authenticate:
-        kvl.firewall_pass = getpass(f"Password for firewall authentication: ")
+        kvl.firewall_pass = getpass(f"\nPassword for firewall authentication: ")
         kvl.is_authenticated = kvl.authenticate(kvl.firewall_pass)
         assert kvl.is_authenticated is True
 
@@ -31,18 +42,19 @@ def test_ssh_key():
         assert kvl.is_ssh_key_valid is True
 
 
-def test_connection_to_servers():
+@pytest.mark.parametrize("server,result", servers_and_results)
+def test_connection_to_servers(server, result):
     if kvl.is_ssh_key_valid is True:
-        vnc_password = None
         vnc_account = kvl.SSH_KEY_ACCOUNT
+        vnc_password = None
     else:
         vnc_account = kvl.args.account
-        vnc_password = getpass(f"Password for user {vnc_account}: ")
+        vnc_password = kvl.vnc_password
 
-    for server in servers_to_test:
-        kvl.log.info(f'Testing SSH to {vnc_account}@{server}.keck.hawaii.edu')
-        output = kvl.do_ssh_cmd('hostname', f'{server}.keck.hawaii.edu',
-                                vnc_account, vnc_password)
-        assert output is not None
-        assert output != ''
-        kvl.log.info(f' Passed')
+    kvl.log.info(f'Testing SSH to {vnc_account}@{server}.keck.hawaii.edu')
+    output = kvl.do_ssh_cmd('hostname', f'{server}.keck.hawaii.edu',
+                            vnc_account, vnc_password)
+    assert output is not None
+    assert output != ''
+    assert output.strip() in [server, result]
+    kvl.log.info(f' Passed')
