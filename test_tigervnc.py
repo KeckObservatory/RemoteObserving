d9afd50c18fc83a1eb94df6f36f502297b8142ef
Josh Walawender <jmwalawender@gmail.com>
2020-04-06 16:46:43 -1000

    Refactor in to two tests, better log messaging

--- test_tigervnc.py
+++ test_tigervnc.py
@@ -16,18 +16,32 @@ kvl.args = create_parser()
 kvl.get_config()
 kvl.check_config()
 
-def test_tigervnc_config():
-    vncviewercmd = kvl.config.get('vncviewer', 'vncviewer')
 
+def we_using_tigervnc():
+    vncviewercmd = kvl.config.get('vncviewer', 'vncviewer')
     cmd = [vncviewercmd, '--help']
     kvl.log.info(f'Checking VNC viewer: {" ".join(cmd)}')
     result = subprocess.run(cmd, capture_output=True)
     output = result.stdout.decode() + '\n' + result.stderr.decode()
-
     if re.search(r'TigerVNC', output):
-        # VNC Viewer is TigerVNC, so we need to verify the RemoteResize config
+        kvl.log.info(f'We are using TigerVNC')
+        return True
+    else:
+        kvl.log.info(f'We are NOT using TigerVNC')
+        return False
+
+
+def test_tigervnc_config_file_exists():
+    if we_using_tigervnc() is True:
         tigervnc_config_file = Path('~/.vnc/default.tigervnc').expanduser()
+        if tigervnc_config_file.exists() is False: 
+            kvl.log.error(f'Could not find {tigervnc_config_file}')
         assert tigervnc_config_file.exists()
+
+
+def test_tigervnc_config_RemoteResize():
+    if we_using_tigervnc() is True:
+        tigervnc_config_file = Path('~/.vnc/default.tigervnc').expanduser()
         with open(tigervnc_config_file) as FO:
             tiger_config = FO.read()
         RRsearch = re.search(r'RemoteResize=(\d)', tiger_config)
