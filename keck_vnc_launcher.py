dc9566eadd10e2ca0a6115f9c9b725a94a1ce4ef
Josh Walawender <jmwalawender@gmail.com>
2020-06-27 09:28:25 -1000

    Bump version

--- keck_vnc_launcher.py
+++ keck_vnc_launcher.py
@@ -26,7 +26,7 @@ import yaml
 import soundplay
 
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 supportEmail = 'remote-observing@keck.hawaii.edu'
 
 SESSION_NAMES = ('control0', 'control1', 'control2',
