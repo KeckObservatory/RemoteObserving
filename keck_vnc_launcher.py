305d69e49ae6fb64982277ba49ac9385fa76adf9
Josh Walawender <joshwalawender@me.com>
2023-03-28 12:27:58 -0700

    bump version

--- keck_vnc_launcher.py
+++ keck_vnc_launcher.py
@@ -28,7 +28,7 @@ import soundplay
 
 
 ## Module vars
-__version__ = '2.0.11'
+__version__ = '2.0.12'
 supportEmail = 'remote-observing@keck.hawaii.edu'
 KRO_API = 'https://www2.keck.hawaii.edu/inst/kroApi.php'
 SESSION_NAMES = ('control0', 'control1', 'control2',
